fe04f94be3775a7be964c22e831a9556470f77e0
Abhinav Jha <abhi.mount.26@gmail.com>
2022-12-16 17:58:52 +0530

    plugins added

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,6 @@
-// module.exports = ({ env }) => ({
-//   'strapi-plugin-populate-deep': {
-//     config: {
-//       defaultDepth: 3, // Default is 5
-//     }
-//   },
-// });
+module.exports = ({ env }) => ({
+  //...
+  'import-export-entries': {
+    enabled: true,
+  },
+});
